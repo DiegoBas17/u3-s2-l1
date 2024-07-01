f5203bfba9028870d7a965be524b5e14b9883e3e
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-07-01 23:22:20 +0200

    add funzioni alla nav per cambiare la categoria

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,48 @@ import MyFooter from "./component/MyFooter";
 import MyNav from "./component/MyNav";
 import Welcome from "./component/Welcome";
 import BookList from "./component/BookList";
+/* *************** */
+import scifi from "./books/scifi.json";
+import fantasy from "./books/fantasy.json";
+import horror from "./books/horror.json";
+import romance from "./books/romance.json";
+import history from "./books/history.json";
 
 class App extends Component {
+  state = {
+    libriDaVisualizzare: fantasy,
+  };
+
+  changeCategory = (category) => {
+    switch (category) {
+      case "Fantasy":
+        this.setState({ libriDaVisualizzare: fantasy });
+        break;
+      case "History":
+        this.setState({ libriDaVisualizzare: history });
+        break;
+      case "Horror":
+        this.setState({ libriDaVisualizzare: horror });
+        break;
+      case "Romance":
+        this.setState({ libriDaVisualizzare: romance });
+        break;
+      case "Scifi":
+        this.setState({ libriDaVisualizzare: scifi });
+        break;
+      default:
+        this.setState({ libriDaVisualizzare: fantasy });
+        break;
+    }
+  };
+
   render() {
     return (
       <div>
-        <MyNav />
+        <MyNav changeCategory={this.changeCategory} />
         <Welcome />
         {/* <AllTheBooks /> */}
-        <BookList />
+        <BookList libriDaVisualizzare={this.state.libriDaVisualizzare} />
         <MyFooter />
       </div>
     );
--- src/component/BookList.jsx
+++ src/component/BookList.jsx
@@ -1,31 +1,18 @@
 import { Component } from "react";
-import Button from "react-bootstrap/Button";
-import scifi from "../books/scifi.json";
-import fantasy from "../books/fantasy.json";
-import horror from "../books/horror.json";
-import romance from "../books/romance.json";
-import history from "../books/history.json";
 import { Container, Form, InputGroup, Row, Col } from "react-bootstrap";
 import SingleBook from "./SingleBook";
 import CommentArea from "./CommentArea";
 
 class BookList extends Component {
   state = {
-    libriDaVisualizzare: fantasy,
     searchInput: "",
-    /* selected: false, */
     idLibroSelezionato: "",
   };
 
   cardSelezionata = (idLibroSelezionato) => {
-    /*  if (this.state.idLibroSelezionato === idLibroSelezionato) {
-      this.setState({ selected: !this.state.selected });
-    } else { */
     this.setState({
-      /*  selected: true, */
       idLibroSelezionato: idLibroSelezionato,
     });
-    /* } */
   };
 
   handleChange = (e) => {
@@ -42,71 +29,27 @@ class BookList extends Component {
             value={this.state.searchInput}
             onChange={this.handleChange}
           />
-          <Button variant="outline-secondary" id="button-addon2">
-            Cerca
-          </Button>
         </InputGroup>
         <h2 className="mb-4">Cambia categoria con un click</h2>
-        <Button
-          className="mx-2 mb-3"
-          variant="primary"
-          onClick={() => this.setState({ libriDaVisualizzare: fantasy })}
-        >
-          Fantasy
-        </Button>
-        <Button
-          className="mx-2 mb-3"
-          variant="primary"
-          onClick={() => this.setState({ libriDaVisualizzare: history })}
-        >
-          History
-        </Button>
-        <Button
-          className="mx-2 mb-3"
-          variant="primary"
-          onClick={() => this.setState({ libriDaVisualizzare: horror })}
-        >
-          Horror
-        </Button>
-        <Button
-          className="mx-2 mb-3"
-          variant="primary"
-          onClick={() => this.setState({ libriDaVisualizzare: romance })}
-        >
-          Romance
-        </Button>
-        <Button
-          className="mx-2 mb-3"
-          variant="primary"
-          onClick={() => this.setState({ libriDaVisualizzare: scifi })}
-        >
-          Scifi
-        </Button>
         <Row className="g-2">
-          <Col lg="8">
-            <Row>
-              {this.state.libriDaVisualizzare
-                .filter((libro) =>
-                  libro.title
-                    .toLowerCase()
-                    .includes(this.state.searchInput.toLowerCase())
-                )
-                .map((libro, index) => {
-                  return (
-                    <Col md="6" key={index}>
-                      <SingleBook
-                        libro={libro}
-                        cardSelezionata={this.cardSelezionata}
-                        isSelected={
-                          this.state.idLibroSelezionato === libro.asin
-                        }
-                      />
-                    </Col>
-                  );
-                })}
-            </Row>
-          </Col>
-          <Col lg="4">
+          {this.props.libriDaVisualizzare
+            .filter((libro) =>
+              libro.title
+                .toLowerCase()
+                .includes(this.state.searchInput.toLowerCase())
+            )
+            .map((libro, index) => (
+              <Col md="6" key={index}>
+                <SingleBook
+                  libro={libro}
+                  cardSelezionata={this.cardSelezionata}
+                  isSelected={this.state.idLibroSelezionato === libro.asin}
+                />
+              </Col>
+            ))}
+        </Row>
+        <Row className="mt-4">
+          <Col>
             {this.state.idLibroSelezionato && (
               <CommentArea idLibro={this.state.idLibroSelezionato} />
             )}
